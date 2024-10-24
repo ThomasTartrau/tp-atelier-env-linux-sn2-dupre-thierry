b0e8e70f6f2f638d05e57dcdf4425a7dbd8ef008
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2024-10-24 15:03:32 +0200

    feat: add ssh_jobs

--- 1.sh
+++ 1.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-WEB=[192.168.122.145]
-BDD=[192.168.122.2]
+# Variables for the IP addresses of the machines
+WEB=john@192.168.122.145
+BDD=john@192.168.122.2
+# Variable for the password of the user root
+SUDOPASS="root"
+
+ssh_jobs()
+{
+    # Add the public key to the authorized_keys file on the remote machine
+    ssh-copy-id -i ~/.ssh/id_ed25519.pub $WEB
+
+    # Disable the password authentication on the remote machine and restart the sshd service
+    ssh -tt $WEB "echo $SUDOPASS | sudo -S sed -i 's/^#\?PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config && sudo systemctl restart sshd"
+}
