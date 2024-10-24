56bf995a6663e4b21f1d1ca3b42daa30d50ba3ad
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2024-10-24 15:14:28 +0200

    some adds

--- 1.sh
+++ 1.sh
@@ -3,8 +3,48 @@
 # Variables for the IP addresses of the machines
 WEB=john@192.168.122.145
 BDD=john@192.168.122.2
+
 # Variable for the password of the user root
 SUDOPASS="root"
+
+# Helper function to display the usage of the script
+help()
+{
+    echo "Usage: ./script.sh [web|bdd|deploywp]"
+}
+
+deploy_web()
+{
+    # install apache2 and some php8
+}
+
+# Check if the user has provided minimum 1 argument
+if [ $# -eq 0 ]; then
+    help
+    exit 1
+fi
+
+case $1 in
+    "web")
+        echo "Web server"
+        ;;
+    "bdd")
+        echo "Database server"
+        ;;
+    "deploywp")
+        echo "Deploy WordPress"
+        ;;
+    *)
+        help
+        ;;
+esac
+
+
+
+
+
+
+
 
 ssh_jobs()
 {
